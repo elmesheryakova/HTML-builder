143be815bbb1e6516a44ff31b2f56f3c218db4b7
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-05-21 13:50:08 +0300

    feat: done 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const pathCopy = path.join(__dirname, 'files-copy');
+const pathFiles= path.join(__dirname,'files');
 
+const copyDir = (pathFiles,pathCopy) => {
+  fs.mkdir(pathCopy,{recursive:true}, err => {
+    if (err) throw err;
+  });
+  fs.readdir(pathFiles, { withFileTypes: true }, (err,files)=> {
+    if (err) throw err;
+    else {
+      files.forEach(file=> {
+        if (file.isFile()) {
+          fs.copyFile(path.join(pathFiles, file.name), path.join(pathCopy, file.name), err => {
+            if (err) throw err;
+          }); 
+        }else {
+          copyDir(path.join(pathFiles, file.name),path.join(pathCopy, file.name));
+        }
+      });
+    } 
+  });
+};
+copyDir(pathFiles, pathCopy);
